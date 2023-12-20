63b772abdaa234ad5e94e1798cbfea4abf93982b
John <john.carden.02@gmail.com>
2023-12-20 06:56:09 -0800

    improved comments

--- WebAPI/internal/store/store.go
+++ WebAPI/internal/store/store.go
@@ -2,8 +2,8 @@ package store
 
 import imp "github.com/jtcarden0001/personacmms/webapi/internal/store/postgres"
 
-// my interfaces are not implementation specific but it's easier keeping everything in one file.
-// I will move these to a common location if I field the idea of another implementation or find a need
+// these embedded interfaces are not implementation specific but it's easier keeping related code together.
+// I will move these to a common location (out of imp) if I field the idea of another implementation or find a need.
 type Store interface {
 	imp.Equipment
 	imp.Tool
