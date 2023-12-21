9d41d8dffdd11245d9565315ace62a37ca8d4802
John <john.carden.02@gmail.com>
2023-12-21 07:25:34 -0800

    added time periodicity unit store code

--- WebAPI/internal/store/postgres/time_periodicity_unit.go
+++ WebAPI/internal/store/postgres/time_periodicity_unit.go
@@ -1,8 +1,6 @@
 package postgres
 
 import (
-	"errors"
-
 	tp "github.com/jtcarden0001/personacmms/webapi/internal/types"
 )
 
@@ -15,21 +13,52 @@ type TimePeriodicityUnit interface {
 }
 
 func (pg *Store) CreateTimePeriodicityUnit(title string) (int, error) {
-	return 0, errors.New("not implemented")
+	query := `INSERT INTO time_periodicity_unit (title) VALUES ($1) returning id`
+	var id int
+	err := pg.db.QueryRow(query, title).Scan(&id)
+
+	return id, err
 }
 
 func (pg *Store) DeleteTimePeriodicityUnit(id int) error {
-	return errors.New("not implemented")
+	query := `DELETE FROM time_periodicity_unit WHERE id = $1`
+	_, err := pg.db.Exec(query, id)
+
+	return err
 }
 
 func (pg *Store) GetAllTimePeriodicityUnit() ([]tp.TimePeriodicityUnit, error) {
-	return nil, errors.New("not implemented")
+	query := `SELECT id, title FROM time_periodicity_unit`
+	rows, err := pg.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var units []tp.TimePeriodicityUnit
+	for rows.Next() {
+		var tpu tp.TimePeriodicityUnit
+		err = rows.Scan(&tpu.Id, &tpu.Title)
+		if err != nil {
+			return nil, err
+		}
+		units = append(units, tpu)
+	}
+
+	return units, nil
 }
 
 func (pg *Store) GetTimePeriodicityUnit(id int) (tp.TimePeriodicityUnit, error) {
-	return tp.TimePeriodicityUnit{}, errors.New("not implemented")
+	query := `SELECT id, title FROM time_periodicity_unit WHERE id = $1`
+	var tpu tp.TimePeriodicityUnit
+	err := pg.db.QueryRow(query, id).Scan(&tpu.Id, &tpu.Title)
+
+	return tpu, err
 }
 
 func (pg *Store) UpdateTimePeriodicityUnit(id int, title string) error {
-	return errors.New("not implemented")
+	query := `UPDATE time_periodicity_unit SET title = $1 WHERE id = $2`
+	_, err := pg.db.Exec(query, title, id)
+
+	return err
 }
