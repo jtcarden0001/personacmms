e8d2333d7d09ea95ff42e548bef071f9d75ad618
John <john.carden.02@gmail.com>
2023-12-21 07:12:28 -0800

    completed store code for task_consumable and task_tool

--- WebAPI/internal/store/postgres/task_consumable.go
+++ WebAPI/internal/store/postgres/task_consumable.go
@@ -1,7 +1,7 @@
 package postgres
 
 import (
-	"errors"
+	"database/sql"
 
 	tp "github.com/jtcarden0001/personacmms/webapi/internal/types"
 )
@@ -10,32 +10,64 @@ type TaskConsumable interface {
 	CreateTaskConsumable(int, int, string) error
 	DeleteTaskConsumable(int, int) error
 	GetAllTaskConsumable() ([]tp.TaskConsumable, error)
-	GetAllTaskConsumableByEquipmentId(int) ([]tp.TaskConsumable, error)
 	GetAllTaskConsumableByTaskId(int) ([]tp.TaskConsumable, error)
-	// GetTaskConsumable(int, int) (tp.TaskConsumable, error)
-	UpdateTaskConsumable(int, int, string) error
+	GetTaskConsumable(int, int) (tp.TaskConsumable, error)
 }
 
 func (pg *Store) CreateTaskConsumable(taskId int, consumableId int, quantity string) error {
-	return errors.New("not implemented")
+	query := `INSERT INTO task_consumable (task_id, consumable_id, quantity_note) VALUES ($1, $2, $3)`
+	_, err := pg.db.Exec(query, taskId, consumableId, quantity)
+
+	return err
 }
 
 func (pg *Store) DeleteTaskConsumable(taskId int, consumableId int) error {
-	return errors.New("not implemented")
+	query := `DELETE FROM task_consumable WHERE task_id = $1 AND consumable_id = $2`
+	_, err := pg.db.Exec(query, taskId, consumableId)
+
+	return err
 }
 
 func (pg *Store) GetAllTaskConsumable() ([]tp.TaskConsumable, error) {
-	return nil, errors.New("not implemented")
-}
+	query := `SELECT task_id, consumable_id, quantity_note FROM task_consumable`
+	rows, err := pg.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-func (pg *Store) GetAllTaskConsumableByEquipmentId(equipmentId int) ([]tp.TaskConsumable, error) {
-	return nil, errors.New("not implemented")
+	return populateTaskConsumableList(rows)
 }
 
 func (pg *Store) GetAllTaskConsumableByTaskId(taskId int) ([]tp.TaskConsumable, error) {
-	return nil, errors.New("not implemented")
+	query := `SELECT task_id, consumable_id, quantity_note FROM task_consumable WHERE task_id = $1`
+	rows, err := pg.db.Query(query, taskId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return populateTaskConsumableList(rows)
 }
 
-func (pg *Store) UpdateTaskConsumable(taskId int, consumableId int, quantity string) error {
-	return errors.New("not implemented")
+func (pg *Store) GetTaskConsumable(consumableId int, quantity string) (tp.TaskConsumable, error) {
+	query := `SELECT task_id, consumable_id, quantity_note FROM task_consumable WHERE task_id = $1 AND consumable_id = $2`
+	var tc tp.TaskConsumable
+	err := pg.db.QueryRow(query, consumableId, quantity).Scan(&tc.TaskId, &tc.ConsumableId, &tc.QuantityNote)
+
+	return tc, err
+}
+
+func populateTaskConsumableList(rows *sql.Rows) ([]tp.TaskConsumable, error) {
+	var taskConsumables []tp.TaskConsumable
+	for rows.Next() {
+		var tc tp.TaskConsumable
+		err := rows.Scan(&tc.TaskId, &tc.ConsumableId, &tc.QuantityNote)
+		if err != nil {
+			return nil, err
+		}
+		taskConsumables = append(taskConsumables, tc)
+	}
+
+	return taskConsumables, nil
 }
--- WebAPI/internal/store/postgres/task_tool.go
+++ WebAPI/internal/store/postgres/task_tool.go
@@ -1,7 +1,7 @@
 package postgres
 
 import (
-	"errors"
+	"database/sql"
 
 	tp "github.com/jtcarden0001/personacmms/webapi/internal/types"
 )
@@ -11,27 +11,63 @@ type TaskTool interface {
 	DeleteTaskTool(int, int) error
 	GetAllTaskTool() ([]tp.TaskTool, error)
 	GetAllTaskToolByTaskId(int) ([]tp.TaskTool, error)
-	GetAllTaskToolByToolId(int) ([]tp.TaskTool, error)
-	// GetTaskTool(int, int) (tp.TaskTool, error)
-	// UpdateTaskTool(int, int) error
+	GetTaskTool(int, int) (tp.TaskTool, error)
 }
 
 func (pg *Store) CreateTaskTool(taskId int, toolId int) error {
-	return errors.New("not implemented")
+	query := `INSERT INTO task_tool (task_id, tool_id) VALUES ($1, $2)`
+	_, err := pg.db.Exec(query, taskId, toolId)
+
+	return err
 }
 
 func (pg *Store) DeleteTaskTool(taskId int, toolId int) error {
-	return errors.New("not implemented")
+	query := `DELETE FROM task_tool WHERE task_id = $1 AND tool_id = $2`
+	_, err := pg.db.Exec(query, taskId, toolId)
+
+	return err
 }
 
 func (pg *Store) GetAllTaskTool() ([]tp.TaskTool, error) {
-	return nil, errors.New("not implemented")
+	query := `SELECT task_id, tool_id FROM task_tool`
+	rows, err := pg.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return populateTaskToolList(rows)
 }
 
 func (pg *Store) GetAllTaskToolByTaskId(taskId int) ([]tp.TaskTool, error) {
-	return nil, errors.New("not implemented")
+	query := `SELECT task_id, tool_id FROM task_tool WHERE task_id = $1`
+	rows, err := pg.db.Query(query, taskId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	return populateTaskToolList(rows)
 }
 
-func (pg *Store) GetAllTaskToolByToolId(toolId int) ([]tp.TaskTool, error) {
-	return nil, errors.New("not implemented")
+func (pg *Store) GetTaskTool(taskId int, toolId int) (tp.TaskTool, error) {
+	query := `SELECT task_id, tool_id FROM task_tool WHERE task_id = $1 AND tool_id = $2`
+	var tc tp.TaskTool
+	err := pg.db.QueryRow(query, taskId, toolId).Scan(&tc.TaskId, &tc.ToolId)
+
+	return tc, err
+}
+
+func populateTaskToolList(rows *sql.Rows) ([]tp.TaskTool, error) {
+	var TaskTools []tp.TaskTool
+	for rows.Next() {
+		var tc tp.TaskTool
+		err := rows.Scan(&tc.TaskId, &tc.ToolId)
+		if err != nil {
+			return nil, err
+		}
+		TaskTools = append(TaskTools, tc)
+	}
+
+	return TaskTools, nil
 }
