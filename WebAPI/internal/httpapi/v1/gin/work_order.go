327380da7414c6182b650da430e4311f88035577
John <john.carden.02@gmail.com>
2023-12-22 13:59:21 -0800

    reduced the route area for workorder to be only heirarchical

--- WebAPI/internal/httpapi/v1/gin/work_order.go
+++ WebAPI/internal/httpapi/v1/gin/work_order.go
@@ -3,23 +3,12 @@ package gin
 import "fmt"
 
 func (h *HttpApi) registerWorkOrderRoutes() {
-	baseRoute := "/v1/work-orders"
-	baseRouteByEquipment := "/v1/equipment/:equipmentId/work-orders"
 	baseRouteByTask := "/v1/equipment/:equipmentId/tasks/:taskId/work-orders"
+	individualRouteByTask := fmt.Sprintf("%s/:workOrderId", baseRouteByTask)
 
-	h.router.POST(baseRoute, h.createWorkOrder)
-	h.router.POST(baseRouteByEquipment, h.createWorkOrderByEquipment)
 	h.router.POST(baseRouteByTask, h.createWorkOrderByTask)
-	h.router.DELETE(fmt.Sprintf("%s/:workOrderId", baseRoute), h.deleteWorkOrder)
-	h.router.DELETE(fmt.Sprintf("%s/:workOrderId", baseRouteByEquipment), h.deleteWorkOrderByEquipment)
-	h.router.DELETE(fmt.Sprintf("%s/:workOrderId", baseRouteByTask), h.deleteWorkOrderByTask)
-	h.router.GET(baseRoute, h.getAllWorkOrder)
-	h.router.GET(fmt.Sprintf("%s/:workOrderId", baseRoute), h.getWorkOrder)
-	h.router.GET(baseRouteByEquipment, h.getAllWorkOrderByEquipment)
-	h.router.GET(fmt.Sprintf("%s/:workOrderId", baseRouteByEquipment), h.getWorkOrderByEquipment)
+	h.router.DELETE(individualRouteByTask, h.deleteWorkOrderByTask)
 	h.router.GET(baseRouteByTask, h.getAllWorkOrderByTask)
-	h.router.GET(fmt.Sprintf("%s/:workOrderId", baseRouteByTask), h.getWorkOrderByTask)
-	h.router.PUT(fmt.Sprintf("%s/:workOrderId", baseRoute), h.updateWorkOrder)
-	h.router.PUT(fmt.Sprintf("%s/:workOrderId", baseRouteByEquipment), h.updateWorkOrderByEquipment)
-	h.router.PUT(fmt.Sprintf("%s/:workOrderId", baseRouteByTask), h.updateWorkOrderByTask)
+	h.router.GET(individualRouteByTask, h.getWorkOrderByTask)
+	h.router.PUT(individualRouteByTask, h.updateWorkOrderByTask)
 }
