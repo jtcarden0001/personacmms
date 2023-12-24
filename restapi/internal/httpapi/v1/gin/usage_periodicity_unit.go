52fe4d7fb0c93e7bd0e76d89766000eabc2f1024
John <john.carden.02@gmail.com>
2023-12-24 08:30:54 -0800

    added usage periodicity unit handler code

--- restapi/internal/httpapi/v1/gin/usage_periodicity_unit.go
+++ restapi/internal/httpapi/v1/gin/usage_periodicity_unit.go
@@ -2,8 +2,10 @@ package gin
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	tp "github.com/jtcarden0001/personacmms/webapi/internal/types"
 )
 
 func (h *HttpApi) registerUsagePeriodicityUnitRoutes() {
@@ -18,26 +20,78 @@ func (h *HttpApi) registerUsagePeriodicityUnitRoutes() {
 }
 
 func (h *HttpApi) createUsagePeriodicityUnit(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	var upu tp.UsagePeriodicityUnit
+	if err := c.BindJSON(&upu); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := h.app.CreateUsagePeriodicityUnit(upu.Title)
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		upu.Id = id
+		c.IndentedJSON(201, upu) // switch to .JSON() for performance
+	}
 }
 
 func (h *HttpApi) deleteUsagePeriodicityUnit(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	upuId, err := strconv.Atoi(c.Param("usagePeriodicityUnitId"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = h.app.DeleteUsagePeriodicityUnit(upuId)
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		c.IndentedJSON(204, gin.H{}) // switch to .JSON() for performance
+	}
 }
 
 func (h *HttpApi) getAllUsagePeriodicityUnit(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	upus, err := h.app.GetAllUsagePeriodicityUnit()
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		c.IndentedJSON(200, upus) // switch to .JSON() for performance
+	}
 }
 
 func (h *HttpApi) getUsagePeriodicityUnit(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	upuId, err := strconv.Atoi(c.Param("usagePeriodicityUnitId"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	upu, err := h.app.GetUsagePeriodicityUnit(upuId)
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		c.IndentedJSON(200, upu) // switch to .JSON() for performance
+	}
 }
 
 func (h *HttpApi) updateUsagePeriodicityUnit(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	upuId, err := strconv.Atoi(c.Param("usagePeriodicityUnitId"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	var upu tp.UsagePeriodicityUnit
+	if err := c.BindJSON(&upu); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	upu.Id = upuId
+	err = h.app.UpdateUsagePeriodicityUnit(upu.Id, upu.Title)
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		c.IndentedJSON(200, upu) // switch to .JSON() for performance
+	}
 }
