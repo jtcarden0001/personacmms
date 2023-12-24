39f4be85e781e6432b488c630e88723b59fcf9c1
John <john.carden.02@gmail.com>
2023-12-24 08:14:49 -0800

    added task handler code

--- restapi/internal/httpapi/v1/gin/task.go
+++ restapi/internal/httpapi/v1/gin/task.go
@@ -2,8 +2,10 @@ package gin
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	tp "github.com/jtcarden0001/personacmms/webapi/internal/types"
 )
 
 func (h *HttpApi) registerTaskRoutes() {
@@ -18,26 +20,95 @@ func (h *HttpApi) registerTaskRoutes() {
 }
 
 func (h *HttpApi) createTask(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	var t tp.Task
+	if err := c.BindJSON(&t); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	equipmentId, err := strconv.Atoi(c.Param("equipmentId"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	id, err := h.app.CreateTask(t.Title, t.Instructions, t.TimePeriodicityQuantity, t.TimePeriodicityUnitId, t.UsagePeriodicityQuantity, t.UsagePeriodicityUnitId, equipmentId)
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		t.Id = id
+		c.IndentedJSON(201, t) // switch to .JSON() for performance
+	}
 }
 
 func (h *HttpApi) deleteTask(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	taskid, err := strconv.Atoi(c.Param("taskId"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = h.app.DeleteTask(taskid)
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		c.IndentedJSON(204, gin.H{}) // switch to .JSON() for performance
+	}
 }
 
 func (h *HttpApi) getAllTaskByEquipment(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	equipmentId, err := strconv.Atoi(c.Param("equipmentId"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	tasks, err := h.app.GetAllTaskByEquipmentId(equipmentId)
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		c.IndentedJSON(200, tasks) // switch to .JSON() for performance
+	}
 }
 
 func (h *HttpApi) getTask(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	taskid, err := strconv.Atoi(c.Param("taskId"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	task, err := h.app.GetTask(taskid)
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		c.IndentedJSON(200, task) // switch to .JSON() for performance
+	}
 }
 
 func (h *HttpApi) updateTask(c *gin.Context) {
-	c.JSON(503, gin.H{"error": fmt.Errorf("not implemented")})
-	return
+	var t tp.Task
+	if err := c.BindJSON(&t); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	equipmentId, err := strconv.Atoi(c.Param("equipmentId"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	taskid, err := strconv.Atoi(c.Param("taskId"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = h.app.UpdateTask(taskid, t.Title, t.Instructions, t.TimePeriodicityQuantity, t.TimePeriodicityUnitId, t.UsagePeriodicityQuantity, t.UsagePeriodicityUnitId, equipmentId)
+	if err != nil {
+		processAppError(c, err)
+	} else {
+		c.IndentedJSON(204, gin.H{}) // switch to .JSON() for performance
+	}
 }
