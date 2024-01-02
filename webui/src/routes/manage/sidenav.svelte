d5cd823000549071f085af99ae1087b2aeafb135
John <john.carden.02@gmail.com>
2024-01-02 08:16:44 -0800

    added destination paths for sidebar nav

--- webui/src/routes/manage/sidenav.svelte
+++ webui/src/routes/manage/sidenav.svelte
@@ -1,11 +1,18 @@
-<script></script>
+<script>
+    let basePath = '/manage';
+    let woPath = basePath + '/workorders';
+    let eqPath = basePath + '/equipment';
+    let taskPath = basePath + '/tasks';
+    let toolPath = basePath + '/tools';
+    let coPath = basePath + '/consumables';
+</script>
 
 <ul class="menu bg-transparent w-56 rounded-box">
-    <li><a>Work Orders</a></li>
-    <li><a>Equipment</a></li>
-    <li><a>Tasks</a></li>
-    <li><a>Tools</a></li>
-    <li><a>Consumables</a></li>
+    <li><a href={woPath}>Work Orders</a></li>
+    <li><a href={eqPath}>Equipment</a></li>
+    <li><a href={taskPath}>Tasks</a></li>
+    <li><a href={toolPath}>Tools</a></li>
+    <li><a href={coPath}>Consumables</a></li>
   </ul>
 
 <style></style>
