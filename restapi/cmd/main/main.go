7352d1222499445a945b4a30633086c1beba2d58
John <john.carden.02@gmail.com>
2024-01-05 07:23:46 -0800

    added path in rest api main function to be able to run rest requests against test db schema

--- restapi/cmd/main/main.go
+++ restapi/cmd/main/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"os"
+
 	"github.com/jtcarden0001/personacmms/webapi/internal/app"
 	"github.com/jtcarden0001/personacmms/webapi/internal/httpapi"
-	"github.com/jtcarden0001/personacmms/webapi/internal/store"
+	st "github.com/jtcarden0001/personacmms/webapi/internal/store"
 )
 
 func main() {
-	storeLayer := store.New()
+	storeLayer := getStore()
 
 	appLayer := app.New(storeLayer)
 
 	httpLayer := httpapi.New(appLayer)
 
 	httpLayer.Start()
+}
+
+// The store has two identical schemas, one for production and one for testing.
+// This function returns the appropriate store based on the environment.
+func getStore() st.Store {
+	testEnv := os.Getenv("personacmmstestenv")
+	if testEnv == "true" {
+		return st.NewTest()
+	}
+
+	return st.New()
 }
