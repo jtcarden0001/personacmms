eaf55256969405bcc4831c51ed64a37f631b9679
John <john.carden.02@gmail.com>
2024-01-08 20:20:18 -0800

    fixed entitytable styling

--- webui/src/lib/components/manage/entitytable.svelte
+++ webui/src/lib/components/manage/entitytable.svelte
@@ -23,14 +23,14 @@
     </div>
     <div class="table-row-group">
       {#each dataRows as row}
-        <a href={basePath.concat("/", row.id, "/view")}>
           <div class="table-row hover:bg-slate-100">
             {#each row.data as cell}
-              <div class="table-cell">{cell}</div>
+              <a class="table-cell" href={basePath.concat("/", row.id, "/view")}>
+                {cell}
+              </a>
             {/each}
             <EntityTableRowSuffix />
           </div>
-        </a>
       {/each}
       <AddNew />
     </div>
