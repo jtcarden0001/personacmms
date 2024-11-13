8bb623ad65e0fda0102a8656979926329d71f1e9
John Carden <john.carden.02@gmail.com>
2024-11-13 08:20:12 -0800

    added a description to the category

--- restapi/internal/store/postgres/category.go
+++ restapi/internal/store/postgres/category.go
@@ -6,16 +6,16 @@ import (
 )
 
 type Category interface {
-	CreateCategory(string) (tp.Category, error)
+	CreateCategory(string, string) (tp.Category, error)
 	DeleteCategory(string) error
 	ListCategory() ([]tp.Category, error)
 	GetCategory(string) (tp.Category, error)
-	UpdateCategory(string, string) (tp.Category, error)
+	UpdateCategory(string, string, string) (tp.Category, error)
 }
 
-func (pg *Store) CreateCategory(title string) (tp.Category, error) {
+func (pg *Store) CreateCategory(title, description string) (tp.Category, error) {
 	id := uid.New()
-	query := `INSERT INTO category (id, title) VALUES ($1, $2) returning id`
+	query := `INSERT INTO category (id, title, description) VALUES ($1, $2, $3) returning id`
 	_, err := pg.db.Exec(query, id.String(), title)
 	if err != nil {
 		return tp.Category{}, err
@@ -68,9 +68,9 @@ func (pg *Store) GetCategory(title string) (tp.Category, error) {
 	return cat, nil
 }
 
-func (pg *Store) UpdateCategory(oldTitle, newTitle string) (tp.Category, error) {
-	query := `UPDATE category SET title = $1 WHERE title = $2`
-	_, err := pg.db.Exec(query, newTitle, oldTitle)
+func (pg *Store) UpdateCategory(oldTitle, newTitle, newDescription string) (tp.Category, error) {
+	query := `UPDATE category SET title = $1, description = $2 WHERE title = $2`
+	_, err := pg.db.Exec(query, newTitle, newDescription, oldTitle)
 	if err != nil {
 		return tp.Category{}, err
 	}
--- restapi/internal/types/category.go
+++ restapi/internal/types/category.go
@@ -1,6 +1,7 @@
 package types
 
 type Category struct {
-	Title string `json:"title" binding:"required"`
-	Id    UUID   `json:"id"`
+	Title       string `json:"title" binding:"required"`
+	Id          UUID   `json:"id"`
+	Description string `json:"description"`
 }
