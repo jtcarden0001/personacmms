0a6c90c2f57781f4e47d81ba3ab3adbf110f6949
John Carden <john.carden.02@gmail.com>
2024-11-13 20:47:50 -0800

    moved commands back into cmd

--- cli/cmd/update/update.go
+++ cli/cmd/update/update.go
@@ -1,18 +1,18 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
-package get
+package update
 
 import (
 	"fmt"
 
-	"github.com/jtcarden0001/personacmms/cli/internal/root"
+	"github.com/jtcarden0001/personacmms/cli/cmd/root"
 	"github.com/spf13/cobra"
 )
 
-// getCmd represents the get command
-var getCmd = &cobra.Command{
-	Use:   "get",
+// updateCmd represents the update command
+var updateCmd = &cobra.Command{
+	Use:   "update",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,20 +21,20 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		fmt.Println("update called")
 	},
 }
 
 func init() {
-	root.RootCmd.AddCommand(getCmd)
+	root.RootCmd.AddCommand(updateCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// updateCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// updateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
