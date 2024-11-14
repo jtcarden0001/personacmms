31b9422c458d8d05b45221ae3b0b6e31b43eca6c
John Carden <john.carden.02@gmail.com>
2024-11-13 21:39:09 -0800

    moved all ver sub commands to print out help message if called directly

--- cli/cmd/get/get.go
+++ cli/cmd/get/get.go
@@ -20,7 +20,10 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		// Print the help message if no subcommands are provided
+		if err := cmd.Help(); err != nil {
+			fmt.Println("Error showing help:", err)
+		}
 	},
 }
 
