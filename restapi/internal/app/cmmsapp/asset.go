195fe14c3a740a2cdd8a7a6779a4610a3bb4464f
John Carden <john.carden.02@gmail.com>
2024-11-30 07:00:36 -0800

    added validations in asset vertical in app layer

--- restapi/internal/app/cmmsapp/asset.go
+++ restapi/internal/app/cmmsapp/asset.go
@@ -3,40 +3,57 @@ package cmmsapp
 import tp "github.com/jtcarden0001/personacmms/restapi/internal/types"
 
 func (a *App) CreateAsset(groupTitle string, asset tp.Asset) (tp.Asset, error) {
-	// TODO: validate group
+	gp, err := a.GetGroup(groupTitle)
+	if err != nil {
+		return tp.Asset{}, err
+	}
 
+	asset.GroupTitle = gp.Title
 	return a.db.CreateAsset(asset)
 }
 
 func (a *App) DeleteAsset(groupTitle string, assetTitle string) error {
-	// TODO: validate group
+	_, err := a.GetGroup(groupTitle)
+	if err != nil {
+		return err
+	}
+
+	_, err = a.GetAsset(groupTitle, assetTitle)
+	if err != nil {
+		return err
+	}
 
 	return a.db.DeleteAsset(groupTitle, assetTitle)
 }
 
 func (a *App) ListAssets(groupTitle string) ([]tp.Asset, error) {
-	// TODO: validate group
-
-	assets, err := a.db.ListAssets()
+	_, err := a.GetGroup(groupTitle)
 	if err != nil {
 		return []tp.Asset{}, err
 	}
 
-	// filter assets by group title
-	var groupAssets []tp.Asset
-	for _, asset := range assets {
-		if asset.GroupTitle == groupTitle {
-			groupAssets = append(groupAssets, asset)
-		}
+	assets, err := a.db.ListAssetsByGroup(groupTitle)
+	if err != nil {
+		return []tp.Asset{}, err
 	}
 
-	return groupAssets, nil
+	return assets, nil
 }
 
 func (a *App) GetAsset(groupTitle string, assetTitle string) (tp.Asset, error) {
+	_, err := a.GetGroup(groupTitle)
+	if err != nil {
+		return tp.Asset{}, err
+	}
+
 	return a.db.GetAsset(groupTitle, assetTitle)
 }
 
 func (a *App) UpdateAsset(groupTitle string, assetTitle string, asset tp.Asset) (tp.Asset, error) {
+	_, err := a.GetGroup(groupTitle)
+	if err != nil {
+		return tp.Asset{}, err
+	}
+
 	return a.db.UpdateAsset(groupTitle, assetTitle, asset)
 }
