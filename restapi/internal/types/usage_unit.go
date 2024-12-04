c03531c9a15173c9ef2abd513d1c15d473ca42e2
John Carden <john.carden.02@gmail.com>
2024-12-03 20:38:08 -0800

    added usage trigger types

--- restapi/internal/types/usage_unit.go
+++ restapi/internal/types/usage_unit.go
@@ -3,9 +3,15 @@ package types
 type UsageUnit struct {
 	Title string `json:"title" binding:"required"`
 	Id    UUID   `json:"id" swaggerignore:"true"`
+	Type  string `json:"type" binding:"required"`
 }
 
 const (
+	// types
+	UsageUnitTypeTime     = "time"
+	UsageUnitTypeDistance = "distance"
+
+	// units
 	// time based
 	UsageUnitHours = "hour"
 	UsageUnitDays  = "day"
