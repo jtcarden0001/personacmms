c21f03cf37e848af28788a77e32255a8cf9f2327
John Carden <john.carden.02@gmail.com>
2024-12-21 01:34:38 +0000

    added start of unit tests for taskconsumable in app

--- restapi/internal/app/cmmsapp/date_trigger_test.go
+++ restapi/internal/app/cmmsapp/date_trigger_test.go
@@ -8,14 +8,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func setupGroupAssetTask(app *App) (tp.Group, tp.Asset, tp.Task) {
+func setupGroupAssetTaskTask(app *App) (tp.Group, tp.Asset, tp.Task, tp.Task) {
 	db := app.db.(*mock.MockStore)
-	group := tp.Group{Title: "group1"}
-	asset := tp.Asset{Title: "asset1", GroupTitle: "group1"}
-	db.CreateGroup(group)
-	db.CreateAsset(asset)
-	task, _ := app.CreateTask("group1", "asset1", tp.Task{Title: "task1"})
-	return group, asset, task
+	group, _ := db.CreateGroup(tp.Group{Title: "group1"})
+	asset, _ := db.CreateAsset(tp.Asset{Title: "asset1", GroupTitle: "group1"})
+	task1, _ := app.CreateTask("group1", "asset1", tp.Task{Title: "task1"})
+	task2, _ := app.CreateTask("group1", "asset1", tp.Task{Title: "task2"})
+	return group, asset, task1, task2
 }
 
 func TestCreateDateTrigger(t *testing.T) {
@@ -23,7 +22,7 @@ func TestCreateDateTrigger(t *testing.T) {
 	app := &App{db: db}
 
 	// setup group, asset, and task
-	_, _, task := setupGroupAssetTask(app)
+	_, _, task, _ := setupGroupAssetTaskTask(app)
 
 	dateTrigger := tp.DateTrigger{TaskId: task.Id}
 	createdDateTrigger, err := app.CreateDateTrigger("group1", "asset1", task.Id.String(), dateTrigger)
@@ -36,7 +35,7 @@ func TestDeleteDateTrigger(t *testing.T) {
 	app := &App{db: db}
 
 	// setup group, asset, and task
-	_, _, task := setupGroupAssetTask(app)
+	_, _, task, _ := setupGroupAssetTaskTask(app)
 	dateTrigger, _ := db.CreateDateTrigger(tp.DateTrigger{TaskId: task.Id})
 
 	err := app.DeleteDateTrigger("group1", "asset1", task.Id.String(), dateTrigger.Id.String())
@@ -48,7 +47,7 @@ func TestListDateTriggers(t *testing.T) {
 	app := &App{db: db}
 
 	// setup group, asset, and task
-	_, _, task := setupGroupAssetTask(app)
+	_, _, task, _ := setupGroupAssetTaskTask(app)
 	db.CreateDateTrigger(tp.DateTrigger{TaskId: task.Id})
 	db.CreateDateTrigger(tp.DateTrigger{TaskId: task.Id})
 
