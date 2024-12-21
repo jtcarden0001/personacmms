02165e788a5845939757642f83872023c0d0c0b1
John Carden <john.carden.02@gmail.com>
2024-12-21 06:21:40 +0000

    added unit tests for work order status in app layer

---
+++ restapi/internal/app/cmmsapp/work_order_test.go
@@ -0,0 +1,71 @@
+package cmmsapp
+
+import (
+	"testing"
+
+	"github.com/jtcarden0001/personacmms/restapi/internal/store/mock"
+	tp "github.com/jtcarden0001/personacmms/restapi/internal/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateWorkOrder(t *testing.T) {
+	db := mock.New()
+	app := &App{db: db}
+
+	group, asset, task := setupGroupAssetTask(app)
+	workOrder := tp.WorkOrder{TaskId: task.Id, StatusTitle: "New"}
+	_, err := app.CreateWorkOrder(group.Title, asset.Title, task.Id.String(), workOrder)
+	assert.NoError(t, err)
+}
+
+func TestDeleteWorkOrder(t *testing.T) {
+	db := mock.New()
+	app := &App{db: db}
+
+	group, asset, task := setupGroupAssetTask(app)
+	workOrder := tp.WorkOrder{TaskId: task.Id, StatusTitle: "New"}
+	workOrder, _ = db.CreateWorkOrder(workOrder)
+
+	err := app.DeleteWorkOrder(group.Title, asset.Title, task.Id.String(), workOrder.Id.String())
+	assert.NoError(t, err)
+}
+
+func TestListWorkOrders(t *testing.T) {
+	db := mock.New()
+	app := &App{db: db}
+
+	group, asset, task := setupGroupAssetTask(app)
+	workOrder1 := tp.WorkOrder{TaskId: task.Id, StatusTitle: "New"}
+	workOrder2 := tp.WorkOrder{TaskId: task.Id, StatusTitle: "In Progress"}
+	db.CreateWorkOrder(workOrder1)
+	db.CreateWorkOrder(workOrder2)
+
+	workOrders, err := app.ListWorkOrders(group.Title, asset.Title, task.Id.String())
+	assert.NoError(t, err)
+	assert.Len(t, workOrders, 2)
+}
+
+func TestGetWorkOrder(t *testing.T) {
+	db := mock.New()
+	app := &App{db: db}
+
+	group, asset, task := setupGroupAssetTask(app)
+	workOrder := tp.WorkOrder{TaskId: task.Id, StatusTitle: "New"}
+	workOrder, _ = db.CreateWorkOrder(workOrder)
+
+	_, err := app.GetWorkOrder(group.Title, asset.Title, task.Id.String(), workOrder.Id.String())
+	assert.NoError(t, err)
+}
+
+func TestUpdateWorkOrder(t *testing.T) {
+	db := mock.New()
+	app := &App{db: db}
+
+	group, asset, task := setupGroupAssetTask(app)
+	workOrder := tp.WorkOrder{TaskId: task.Id, StatusTitle: "New"}
+	workOrder, _ = db.CreateWorkOrder(workOrder)
+
+	workOrder.StatusTitle = "In Progress"
+	_, err := app.UpdateWorkOrder(group.Title, asset.Title, task.Id.String(), workOrder.Id.String(), workOrder)
+	assert.NoError(t, err)
+}
